0c51c8e8e552d01be5a2de00202faa2d610faf98
baiiu <baiiu@foxmail.com>
2020-01-13 23:20:34 +0800

    config

--- plungin/src/main/kotlin/com/baiiu/module/ModulePlugin.kt
+++ plungin/src/main/kotlin/com/baiiu/module/ModulePlugin.kt
@@ -21,53 +21,41 @@ class ModulePlugin : Plugin<Project> {
 //    var isFirst: Boolean = true
 
     override fun apply(project: Project) {
-
-
         println("ModulePlugin#apply: " + project.name + ", " + project.displayName)
 
         if (!project.plugins.hasPlugin("com.android.application")) {
             throw GradleException("ModulePlugin must be used in Android Application plugin")
         }
 
-//        project.extensions.add("module", ModuleExtension::class.java)
-//        val moduleExtension: ModuleExtension = project.extensions.getByType(ModuleExtension::class.java)
-//        println("moduleExtension: " + moduleExtension.modules)
-
         val assembleTask = isAssemble(project.gradle.startParameter.taskNames)
         println("taskNames: " + assembleTask + ", " + project.gradle.startParameter.taskNames)
 
-
         val transfrom = ModuleTransform()
 
+        val runtimeOnlyConfiguration = project.configurations.getByName("runtimeOnly")
+        for (dependency in runtimeOnlyConfiguration.dependencies) {
+            println("implementationConfiguration: " + runtimeOnlyConfiguration.dependencies + ", " + dependency)
+        }
 
-        project.afterEvaluate {
+        val jsonFile = File(project.rootDir, "runalone.json")
+        if (!jsonFile.exists()) {
+            return
+        }
+        val json = jsonFile.readText()
+        println("json: $json")
 
-            val runtimeOnlyConfiguration = project.configurations.getByName("runtimeOnly")
-            for (dependency in runtimeOnlyConfiguration.dependencies) {
-                println("implementationConfiguration: " + runtimeOnlyConfiguration.dependencies + ", " + dependency)
-            }
+        val list: List<RunAlone> = Gson().fromJson(json, object : TypeToken<List<RunAlone?>?>() {}.type)
+        println("runAloneList: $list")
 
-            val jsonFile = File(project.rootDir, "runalone.json")
-            if (!jsonFile.exists()) {
-                return@afterEvaluate
-            }
-            val json = jsonFile.readText()
-            println("json: $json")
+        list.forEach { item ->
+            if (project.name.contains(item.module, true)) {
+                transfrom.mRunalone = item
 
-            val list: List<RunAlone> = Gson().fromJson(json, object : TypeToken<List<RunAlone?>?>() {}.type)
-            println("runAloneList: $list")
-
-            list.forEach { item ->
-                if (project.name.contains(item.module, true)) {
-                    transfrom.mRunalone = item
-
-                    item.implement.forEach {
-                        println("addImplementation: $it")
-                        runtimeOnlyConfiguration.dependencies.add(project.dependencies.create(project.project(it)))
-                    }
+                item.implement.forEach {
+                    println("addImplementation: $it")
+                    runtimeOnlyConfiguration.dependencies.add(project.dependencies.create(project.project(it)))
                 }
             }
-
         }
 
         val android = project.extensions.getByType(AppExtension::class.java)
